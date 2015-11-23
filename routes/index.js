1aee91c2d86ba36a3bdd4b9010172321d9c90c8e
danil zakablukovskii <danil.kabluk@gmail.com>
2015-11-24 00:28:46 +0100

    fix?

--- routes/index.js
+++ routes/index.js
@@ -8,20 +8,14 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/auth', passport.authenticate('provider', {scope: ['like', 'logged_in']}), function (req, res) {
-    res.json({
-        1: req,
-        2: res
-    });
+    res.json(res.user);
 });
 
 router.get('/coub', passport.authenticate('provider', {
         //successRedirect: '/',
         //failureRedirect: '/'
     }), function (req, res) {
-        res.json({
-            1: req,
-            2: res
-        })
+        res.json(res.user);
     }
 );
 
