61c11c88c15dd38b0ccec442f5b4161d874907eb
danil zakablukovskii <danil.kabluk@gmail.com>
2015-11-23 23:46:01 +0100

    try passport

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('../lib/coub-strategy');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
+
+router.get('/auth', passport.authenticate('provider', {scope: ['like', 'logged_in']}));
+
+router.get('/coub', passport.authenticate('provider', {
+    successRedirect: '/',
+    failureRedirect: '/'
+}));
 
 module.exports = router;
