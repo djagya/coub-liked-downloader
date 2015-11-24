b01a597f62a4c1fec36842cd92355aea8874ae8d
danil zakablukovskii <danil.kabluk@gmail.com>
2015-11-24 21:35:36 +0100

    rework add features

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,67 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('../lib/coub-strategy');
-
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+var request = require('request').defaults({
+    baseUrl: 'http://coub.com/api/v2/'
 });
 
-router.get('/success', function (req, res, next) {
+router.get('/', function (req, res) {
+    res.render('index');
+});
+
+router.route('/start')
+    // page to start a download
+    .get(function (req, res) {
+        res.render('start');
+    })
+
+    // form with email and quality to start an async job
+    .post(function (req, res) {
+        // todo start async work
+        // todo get email, quality
+
+        request.get({
+            uri: '/likes/by_channel',
+            qs: {
+                channel_id: req.user.channel_id,
+                access_token: req.user.access_token
+            }
+        }, {timeout: 1500}, function (error, response, body) {
+            if (error) {
+                console.log(error);
+                res.render('error', {error: error});
+            }
+
+            if (response.statusCode != 200) {
+                console.log('Error: Status code ' + response.statusCode + ', body: ' + body);
+                res.render('error', {error: error});
+            }
+
+            res.send(response);
+        });
+    });
+
+// get prepared archive
+router.get('/download/:id', function (req, res) {
+    // check files if there is an archive with that id
+    // req.params.id
+
+    res.send({file: 'download'});
+});
+
+router.get('/success', function (req, res) {
+    // todo make a request
+    // go through coubs
+    // download to a folder
+    // each should have name
+
+
+    // todo: do not download files, use remote file urls for ffmpeg and ffprobe
+    // how to download:
+    // 1. get video
+    // 2. get audion
+    // 3. check audio (with ffprobe: ffprobe -i input.mp4 -show_entries format=duration -v quiet -of csv="p=0") and video duarations
+    // 4. if audio > video: ffmpeg -f concat -i list.txt -i input.mp3 -c copy output.mp4 , where list.txt is the repeated video names like "file 'input.mp4'" on each line n times, where n = audio_duration/video_duration
     res.json({
         message: 'success',
         session: JSON.stringify(req.session),
@@ -17,8 +71,8 @@ router.get('/success', function (req, res, next) {
 
 router.get('/auth', passport.authenticate('provider', {scope: ['like', 'logged_in']}));
 
-router.get('/coub', passport.authenticate('provider', {
-    successRedirect: '/success',
+router.get('/callback', passport.authenticate('provider', {
+    successRedirect: '/start',
     failureRedirect: '/'
 }));
 
