e7f7a20384124f5dae12b3b3b9a4a9448bd1fc1c
danil zakablukovskii <danil.kabluk@gmail.com>
2015-11-25 00:14:35 +0100

    finish page

--- routes/index.js
+++ routes/index.js
@@ -36,50 +36,52 @@ router.route('/start')
             coubsData = [];
 
         // async loop to fetch liked coubs
-        async.doWhilst(function (cb) {
-            request.get('/likes/by_channel', {
-                qs: {
-                    channel_id: req.user.channel_id,
-                    page: page,
-                    access_token: req.user.accessToken
-                },
-                timeout: 1500
-            }, function (error, response, body) {
-                if (error) {
-                    console.log(error);
-                    res.render('error', {message: 'Error', error: error});
-                    cb('API error');
-                }
+        //async.doWhilst(function (cb) {
+        //    request.get('/likes/by_channel', {
+        //        qs: {
+        //            channel_id: req.user.channel_id,
+        //            page: page,
+        //            access_token: req.user.accessToken
+        //        },
+        //        timeout: 1500
+        //    }, function (error, response, body) {
+        //        if (error) {
+        //            console.log(error);
+        //            res.render('error', {message: 'Error', error: error});
+        //            cb('API error');
+        //        }
+        //
+        //        if (response.statusCode != 200) {
+        //            console.log('Error: Status code ' + response.statusCode + ', body: ' + body);
+        //            res.render('error', {message: body, error: {}});
+        //            cb('API error');
+        //        }
+        //
+        //        var jsonResult = JSON.parse(body);
+        //
+        //        _.each(jsonResult.coubs, function (coub) {
+        //            coubsData.push({
+        //                id: coub.permalink,
+        //                title: coub.title,
+        //                video: coub.file_versions.web,
+        //                audio: coub.audio_versions
+        //            });
+        //        });
+        //
+        //        totalPages = jsonResult.total_pages;
+        //        page++;
+        //
+        //        // call callback to iterate further
+        //        cb();
+        //    });
+        //}, function () {
+        //    return page < totalPages;
+        //}, function (err) {
+        //    console.log(err);
+        //    res.render('info', {data: coubsData});
+        //});
 
-                if (response.statusCode != 200) {
-                    console.log('Error: Status code ' + response.statusCode + ', body: ' + body);
-                    res.render('error', {message: body, error: {}});
-                    cb('API error');
-                }
-
-                var jsonResult = JSON.parse(body);
-
-                _.each(jsonResult.coubs, function (coub) {
-                    coubsData.push({
-                        id: coub.permalink,
-                        title: coub.title,
-                        video: coub.file_versions.web,
-                        audio: coub.audio_versions
-                    });
-                });
-
-                totalPages = jsonResult.total_pages;
-                page++;
-
-                // call callback to iterate further
-                cb();
-            });
-        }, function () {
-            return page < totalPages;
-        }, function (err) {
-            console.log(err);
-            res.render('info', {data: coubsData});
-        });
+        res.render('success');
     });
 
 // get prepared archive
