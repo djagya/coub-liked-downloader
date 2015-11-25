324eb27b0cca30ee622ee03dda39e76ccc6246a7
danil zakablukovskii <danil.kabluk@gmail.com>
2015-11-25 01:51:00 +0100

    console log

--- worker.js
+++ worker.js
@@ -8,6 +8,8 @@ var request = require('request').defaults({
     baseUrl: 'http://coub.com/api/v2/'
 });
 
+console.log('Worker started');
+
 // process queue
 queue.process('download_coubs', 5, function (job, done) {
     getCoubs(job.data.channel_id, job.data.access_token, function (data) {
@@ -23,6 +25,7 @@ queue.process('download_coubs', 5, function (job, done) {
             coubsData = [];
 
         job.log('Getting likes for %d channel', channelId);
+        console.log('Getting likes for %d channel', channelId);
 
         // async loop to fetch liked coubs
         async.doWhilst(function (cb) {
@@ -58,6 +61,7 @@ queue.process('download_coubs', 5, function (job, done) {
                 totalPages = jsonResult.total_pages;
 
                 job.log('Page %d done', page);
+                console.log('Page %d done', page);
                 page++;
 
                 // update job progress (make it so that when all coubs loaded = 10%, since processing is another 90%)
@@ -77,12 +81,14 @@ queue.process('download_coubs', 5, function (job, done) {
 
     function processCoubs(data, quality, cb) {
         job.log('Processing %d coubs with %s quality', data.length, quality);
+        console.log('Processing %d coubs with %s quality', data.length, quality);
 
         cb();
     }
 
     function sendEmail(to) {
         job.log('Sending email to %s', to);
+        console.log('Sending email to %s', to);
 
         cb();
     }
