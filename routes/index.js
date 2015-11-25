fad788abe4603a8fc7e3dc064c1f4041b314b1ac
danil zakablukovskii <danil.kabluk@gmail.com>
2015-11-25 17:28:17 +0100

    fix

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,8 @@ var router = express.Router();
 var passport = require('../lib/coub-strategy');
 var _ = require('lodash');
 var qualities = require('../helpers/qualityList');
-//var redis = require('redis'),
-//    client = redis.createClient({
-//        url: process.env.REDIS_URL
-//    });
+var redis = require('redis'),
+    client = redis.createClient(process.env.REDIS_URL, {});
 var kue = require('kue'),
     queue = kue.createQueue({
         redis: process.env.REDIS_URL
