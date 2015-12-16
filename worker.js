7e4e79792c27720162258fa74fa2fd3b6d284e5c
danil zakablukovskii <danil.kabluk@gmail.com>
2015-12-16 01:16:03 +0100

    exctract getCoubs

--- worker.js
+++ worker.js
@@ -3,14 +3,11 @@
 const POPULAR_COUB_LIKES_COUNT = 1000;
 const FOLDER_SOURCES = 'data/sources';
 const FOLDER_DONE = 'data/coubs';
-const CACHE_DURATION = 3600;
 
 var kue = require('kue'),
     queue = kue.createQueue({
         redis: process.env.REDIS_URL
     });
-var redis = require('redis'),
-    client = redis.createClient(process.env.REDIS_URL, {});
 var _ = require('lodash');
 var async = require('async');
 var nodemailer = require('nodemailer');
@@ -20,6 +17,7 @@ var exec = require('child_process').exec,
 var request = require('request');
 var rmdir = require('rimraf');
 var archiver = require('archiver');
+var getCoubs = require('./helpers/getCoubs');
 
 console.log('Worker started');
 
@@ -43,7 +41,12 @@ queue.process('download_coubs', 5, function (job, done) {
 
     async.waterfall([
         function (cb) {
-            return getCoubs(job.data.channel_id, job.data.access_token, cb);
+            var result = getCoubs(job.data.channel_id, job.data.access_token, cb);
+
+            // update job progress (make it so that when all coubs loaded = 15%, since email sending = 5% and processing is another 80%)
+            job.progress(10, 100);
+
+            return result;
         },
         function (data, cb) {
             return processCoubs(data, cb);
@@ -60,100 +63,6 @@ queue.process('download_coubs', 5, function (job, done) {
 
         done();
     });
-
-    /**
-     * Fetch Coubs by channel id and create needed data array
-     * @param channelId
-     * @param accessToken
-     * @param cb
-     */
-    function getCoubs(channelId, accessToken, cb) {
-        var page = 1,
-            totalPages = 1,
-            coubsData = [],
-            cacheKey = `coub_api_data:${job.data.channel_id}`;
-
-        console.log('Getting likes for %d channel', channelId);
-
-        // check cache
-        client.get(cacheKey, function (err, res) {
-            if (res) {
-                return cb(null, JSON.parse(res));
-            }
-        });
-
-        // async loop to fetch liked coubs
-        async.doWhilst(function (cb) {
-            request.get('http://coub.com/api/v2/likes/by_channel', {
-                qs: {
-                    channel_id: channelId,
-                    page: page,
-                    access_token: accessToken
-                },
-                timeout: 10000
-            }, function (error, response, body) {
-                if (error) {
-                    console.log(error);
-                    return cb('API error');
-                }
-
-                if (response.statusCode !== 200) {
-                    console.log('Error: Status code ' + response.statusCode + ', body: ' + body);
-                    return cb('API error');
-                }
-
-                /** @see http://coub.com/dev/docs/Coub+API/Data+stuctures **/
-                var jsonResult = JSON.parse(body);
-
-                _.each(jsonResult.coubs, function (coub) {
-                    var preparedData = {
-                        id: coub.permalink,
-                        title: coub.title,
-                        video: coub.file_versions.web,
-                        likesCount: coub.likes_count,
-                        duration: coub.duration
-                    };
-
-                    // remove 'big' quality for video
-                    _.remove(preparedData.video.versions, function (version) {
-                        return version === 'big';
-                    });
-
-                    // if coub has audio (otherwise video by itself can have builtin audio)
-                    if (Object.keys(coub.audio_versions).length) {
-                        // remove not needed chunks property and get only mid version
-                        preparedData.audio = {
-                            template: coub.audio_versions.template,
-                            version: coub.audio_versions.versions[1] || coub.audio_versions.versions[0]
-                        };
-                    }
-
-                    coubsData.push(preparedData);
-                });
-
-                console.log('Page %d done', page);
-
-                totalPages = jsonResult.total_pages;
-                page++;
-
-                // update job progress (make it so that when all coubs loaded = 15%, since email sending = 5% and processing is another 80%)
-                job.progress(page * jsonResult.per_page, (jsonResult.per_page * totalPages * 100) / 15);
-
-                // call callback to iterate further
-                cb();
-            });
-        }, function () {
-            return page < totalPages;
-        }, function (err) {
-            if (err) {
-                cb(err);
-            } else {
-                client.setex(cacheKey, CACHE_DURATION, JSON.stringify(coubsData), function (err, res) {
-                    cb(err, coubsData);
-                });
-            }
-        });
-    }
 
     /**
      * @param data structure: {
