8a0bcbec289fbd2db3d660890f65ba9b4535d41b
danil zakablukovskii <danil.kabluk@gmail.com>
2016-02-03 16:44:38 +0100

    gg

--- app.js
+++ app.js
@@ -1,74 +1,35 @@
-var express = require('express');
+#!/usr/bin/env node
+'use strict';
+
+var program = require('commander');
+
+var opener = require('opener');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var expressValidator = require('express-validator');
-var session = require('express-session');
 var passport = require('./lib/coub-strategy');
-var RedisStore = require('connect-redis')(session);
+var pkg = require(path.join(__dirname, 'package.json'));
 
-var routes = require('./routes/index');
+// vars
+var savePath = './data';
 
-var app = express();
+// program
+program
+    .version(pkg.version)
+    .option('-p, --path', 'save path')
+    .option('-q, --quality <low|mid|high>', 'quality', /^(low|mid|high)$/, 'mid')
+    .parse(process.argv);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var quality = program.quality;
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(expressValidator());
-app.use(cookieParser('nfkaldksjaflmsfjbeuk1471829rhr9j0!J8r20r'));
-app.use(session({
-    store: new RedisStore({
-        url: process.env.REDIS_URL
-    }),
-    secret: 'nfkaldksjaflmsfjbeuk1471829rhr9j0!J8r20r',
-    resave: false,
-    saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+if (program.path) {
+    savePath = program.path;
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+// todo:
+// get access token
+
+// check if ffmpg is installed
+// verbose mode to show ffmpg output
 
 
-module.exports = app;
